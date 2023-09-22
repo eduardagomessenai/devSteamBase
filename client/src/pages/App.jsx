2b429e376889758b60ade20b13879f1fe2fea0f2
dudsssenai <eduardagomesdesouza8@gmail.com>
2023-09-22 11:24:56 -0300

    Criado componentes- container e subtitle.

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,3 +1,5 @@
+import Container from "../components/container/container";
+import Subtitle from "../components/forms/subtitle/subtitle";
 import Nav from "../components/nav/Nav";
 import "./App.css";
 
@@ -5,6 +7,10 @@ const App = () => {
   return (
     <div>
       <Nav />
+
+      <Container>
+        <Subtitle>promoções</Subtitle>
+      </Container>
     </div>
   );
 };
